7a2f50afb8518b4f9df628a6241c424cb2e608ed
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-04 13:50:56 +0530

    Add files via upload

---
+++ DA1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array1 = np.array([1,3,5])
+print("np.array():\n" , array1)
+
+array2 = np.zeros((3 , 3))
+print("\nnp.zeros():\n" , array2)
+
+array3 = np.ones((2,4))
+print("\nnp.ones():\n",array3)
