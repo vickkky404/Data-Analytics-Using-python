f4958b5b218d856b0229d2b1d7271ea6c44a332f
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-04 14:08:15 +0530

    Add files via upload

---
+++ Mathmatical Function.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array1 = np.array([1,2,3,4,5])
+array2 = np.array([4,9,16,25,36])
+
+arr_sum = np.add(array1 , array2)
+arr_diff = np.subtract(array1 , array2)
+arr_sqt = np.sqrt(array1 , array2)
+
+print("\nSum of array:\n" , arr_sum)
+print("\nDiffrence of array\n" , arr_sum) 
+print("\nSquare root of first array:\n" , arr_sqt)
