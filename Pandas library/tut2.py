16dc94f8a81d252319d7b00c327aaa7a7144b8aa
nalin <nalinikantbastia@gmail.com>
2026-01-06 10:25:58 +0530

    pandas tutorial 2

--- Pandas library/tut2.py
+++ Pandas library/tut2.py
@@ -1,12 +1,20 @@
 # creaye a list , print it , convert list to series and then print the series , create a homogenious array and then check the types
 
 import pandas as pd
+import numpy as np
+
+
 num = [1, 2, 3, 4, 5]
 print("List:", num)
+
 s = pd.Series(num)
+
+
 print("Series:\n", s)
 print("Type of Series:", type(s))
-import numpy as np
+
 homogeneous_array = np.array([10, 20, 30, 40, 50])
+
+
 print("Homogeneous Array:", homogeneous_array)
 print("Type of Homogeneous Array:", type(homogeneous_array))
