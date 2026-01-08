6aa0649b884c9c11cb17cf160fe1c881efff289f
nalin <nalinikantbastia@gmail.com>
2026-01-08 10:36:56 +0530

    pandas tutorial 4

--- Pandas library/tut3.py
+++ Pandas library/tut3.py
@@ -5,3 +5,8 @@ my_list = [5,3,4,2,1]
 s = pd.Series(my_list)
 print(s)
 print(type(s))
+# create a numpy array from the list
+import numpy as np
+arr = np.array(my_list)
+print(arr)
+print(type(arr))
